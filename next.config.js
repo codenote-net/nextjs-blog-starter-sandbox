1f4d4311a81cd703a431a634be08a44749a54aa5
Tadashi Shigeoka <shigeoka@codenote.net>
2023-08-18 23:04:11 +0900

    [DNM] fix assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /**
  * @type {import('next').NextConfig}
  */
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/nextjs-blog-starter-sandbox' : undefined,
+  // assetPrefix: isProd ? '/nextjs-blog-starter-sandbox' : undefined,
+  assetPrefix: '/nextjs-blog-starter-sandbox',
 }
