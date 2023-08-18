2abb19261a8db6296f181d8b6088cbc31b1162ee
Tadashi Shigeoka <shigeoka@codenote.net>
2023-08-18 22:28:36 +0900

    Change `assetPrefix` in production env for GitHub Pages
    - [next\.config\.js Options: assetPrefix \| Next\.js](https://nextjs.org/docs/pages/api-reference/next-config-js/assetPrefix)

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/nextjs-blog-starter-sandbox' : undefined,
+}
